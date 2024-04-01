7d921e6fec5fd0d525c29bae827d60e2ea4a9fa4
paturikaustubh <plutofeb115@gmail.com>
2024-04-01 13:36:48 +0530

    server ip from .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { config } from "dotenv";
 
-// https://vitejs.dev/config/
+config({ path: __dirname + "/.env" });
+const { SERVER_IP } = process.env;
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:6969/",
+        target: `http://${SERVER_IP}:6969/`,
         changeOrigin: true,
       },
     },
